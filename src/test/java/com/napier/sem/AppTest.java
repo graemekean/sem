8adbbf30e2b3fd07933d41d0f5625e676ee7dd21
graemekean <graemekean@gmail.com>
2020-03-17 13:54:33 +0000

    changed unit test file

--- src/test/java/com/napier/sem/AppTest.java
+++ src/test/java/com/napier/sem/AppTest.java
@@ -10,14 +10,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest
 {
-    static App app;
-
-    @BeforeAll
-    static void init()
-    {
-        app = new App();
-
-    }
+//    static App app;
+//
+//    @BeforeAll
+//    static void init()
+//    {
+//        app = new App();
+//
+//    }
 
 //    @Test
 //    void CountryReport()
